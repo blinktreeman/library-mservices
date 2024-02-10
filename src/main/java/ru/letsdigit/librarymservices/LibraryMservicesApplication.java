b40cc1f8150bef46ee6e6a735713f0e4c7aaef92
Eugene Elantsev <blinktreeman@gmail.com>
2024-02-10 22:16:12 +0700

    initial commit

---
+++ src/main/java/ru/letsdigit/librarymservices/LibraryMservicesApplication.java
@@ -0,0 +1,13 @@
+package ru.letsdigit.librarymservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/letsdigit/librarymservices/LibraryMservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.letsdigit.librarymservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
